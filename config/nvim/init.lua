447a920a5fb3e861c81c55f8af546cb2ed86e6a8
sbite0138 <24842740+sbite0138@users.noreply.github.com>
2024-02-08 17:36:47 +0000

    update lua.init

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,3 +10,12 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {}
+    end,
+    }
+})
