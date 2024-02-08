e683a7fc75bc27e8e442369a5f6dd1d39432d2b8
sbite0138 <24842740+sbite0138@users.noreply.github.com>
2024-02-08 18:33:27 +0000

    add plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,30 @@
+-- basic settings
+local opt = vim.opt
+-- 「※」等の記号を打つと、半角文字と重なる問題がある。「※」などを全角文字の幅で表示するために設定する
+opt.ambiwidth = 'double'
+
+-- 新しい行を改行で追加した時に、ひとつ上の行のインデントを引き継がせます。
+opt.autoindent = true
+opt.smartindent = true
+-- smartindent と cindent を両方 true にしたときは、cindent のみ true になるようです。
+-- opt.cindent = true
+-- カーソルが存在する行にハイライトを当ててくれます。
+opt.cursorline = true
+
+-- カーソルが存在する列にハイライトを当てたい場合、下記をtrueにする。
+-- opt.cursorculumn = true
+
+-- TABキーを押した時に、2文字分の幅を持ったTABが表示されます。
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+-- tabstop で設定した数の分の半角スペースが入力されます。
+opt.expandtab = true
+-- カーソル行からの相対的な行番号を表示する
+opt.number = true
+opt.termguicolors = true
+
+-- lazy nvim and plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,11 +38,29 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{
+  {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config=true
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config=true,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy=false,
+    priority=1000,
     config = function()
-        require('lualine').setup {}
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
     end,
-    }
+  }
 })
