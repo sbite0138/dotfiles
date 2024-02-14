5178a689df108ab373161ca548a55f29a986c930
sbite0138 <24842740+sbite0138@users.noreply.github.com>
2024-02-14 17:40:17 +0900

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,80 +24,173 @@ opt.expandtab = true
 opt.number = true
 opt.termguicolors = true
 
-
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  -- vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- -- delay update diagnostics
+-- vim.lsp.diagnostic.on_publish_diagnostics, {
+-- -- delay update diagnostics
 --     update_in_insert = true,
 --   }
 -- )
-opt.signcolumn = 'yes' 
+opt.signcolumn = 'yes'
 
 -- lazy nvim and plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
- {
+require("lazy").setup({{
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim", "3rd/image.nvim" -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    config=true,
-  },
-  {
+    config = true
+}, {
     "folke/tokyonight.nvim",
-    lazy=false,
-    priority=1000,
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
+    end
+}, {
     "neovim/nvim-lspconfig",
-    cmd = { "LspInfo", "LspLog" },
-    event = { "BufRead" },
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall" },
-    event = { "WinNew", "WinLeave", "BufRead" },
-    config = true  },
-  { "williamboman/mason-lspconfig.nvim" },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-  },
-  {'nvim-treesitter/nvim-treesitter'},
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config=true
-  },
- })
+    cmd = {"LspInfo", "LspLog"},
+    event = {"BufRead"}
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-  function (server_name)
-    require("lspconfig")[server_name].setup {
-     on_attach = on_attach
-    }
-  end,
-}
-vim.api.nvim_set_keymap('n', '<Leader>f', [[<Cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({}))<CR>]], { noremap = true, silent = true })
+}, {
+    "williamboman/mason.nvim",
+    cmd = {"Mason", "MasonInstall"},
+    event = {"WinNew", "WinLeave", "BufRead"},
+    config = function()
+        require("mason").setup()
+    end
+}, {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {'hrsh7th/cmp-nvim-lsp'},
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup_handlers {function(server_name)
+            require("lspconfig")[server_name].setup {
+                on_attach = on_attach,
+                capabilities = capabilities
+            }
+        end}
+    end
+}, {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fF', builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fg', builtin.grep_string, {})
+      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>fp", builtin.registers, {})
+      -- vim.keymap.set('n', '<leader>fw', require('telescope').extensions.windows.list, {})
+    end
+}, {'nvim-treesitter/nvim-treesitter'}, {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {'nvim-tree/nvim-web-devicons'},
+    config = true
+}, {
+    'hrsh7th/nvim-cmp',
+    config = function()
+        local cmp = require 'cmp'
+        cmp.setup({
+            snippet = {
+                -- REQUIRED - you must specify a snippet engine
+                expand = function(args)
+                    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                end
+            },
+            window = {
+                -- completion = cmp.config.window.bordered(),
+                -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-j>'] = cmp.mapping.select_next_item(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<CR>'] = cmp.mapping.confirm {
+                    select = false
+                },
+                ['<C-e>'] = cmp.mapping.abort()
+            }),
+            sources = cmp.config.sources({{
+                name = 'nvim_lsp'
+            }, --  {
+            --     name = 'vsnip'
+            -- } 
+            -- For vsnip users.
+            {
+                name = 'luasnip'
+            } -- For luasnip users.
+            -- { name = 'ultisnips' }, -- For ultisnips users.
+            -- { name = 'snippy' }, -- For snippy users.
+            }, {{
+                name = 'buffer'
+            }})
+        })
+
+        -- Set configuration for specific filetype.
+        cmp.setup.filetype('gitcommit', {
+            sources = cmp.config.sources({{
+                name = 'git'
+            } -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+            }, {{
+                name = 'buffer'
+            }})
+        })
+
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({'/', '?'}, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {{
+                name = 'buffer'
+            }}
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({{
+                name = 'path'
+            }}, {{
+                name = 'cmdline'
+            }})
+        })
+
+        -- Set up lspconfig.
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+        require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+            capabilities = capabilities
+        }
+    end,
+    lazy = true,
+    event = "InsertEnter",
+    dependencies = { -- apearance
+    {
+        'onsails/lspkind.nvim',
+        dependencies = {'nvim-tree/nvim-web-devicons'}
+    }, -- source
+    {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-nvim-lsp-signature-help'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'}, {'hrsh7th/cmp-nvim-lsp-document-symbol'}, {"f3fora/cmp-spell"}, {"ray-x/cmp-treesitter"},
+    -- snippet
+    {'L3MON4D3/LuaSnip'}, {'saadparwaiz1/cmp_luasnip'}}
+}})
+
+-- require("mason").setup()
+-- require("mason-lspconfig").setup()
 
